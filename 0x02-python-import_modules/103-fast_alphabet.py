d8bd3089b056c3ae8ac367cb77225d89b46f5878
Azure0922 <maryam44keks@gmail.com>
2023-01-05 11:23:43 -0800

    varload

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("ABCDEFGHIJKLMNOPQRSTUVWXYZ", end='')
-print()
+caps = list(map(chr, range(65, 91)))
+print(*caps, sep='')
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+import variable_load_5
+c = variable_load_5.a
+if __name__ == '__main__':
+    print(c)
