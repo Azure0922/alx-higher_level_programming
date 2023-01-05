26236c18089d4eff4af0bd2c8e9f381699f73834
Azure0922 <maryam44keks@gmail.com>
2023-01-05 09:29:25 -0800

    addpy

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,5 +2,6 @@
 from add_0 import add
 a = 1
 b = 2
-print("{c} + {d} = ".format(c=a, d=b), end='')
-add(a, b)
+e = add(a, b)
+if __name__ == '__main__':
+    print("{c} + {d} = {f}".format(c=a, d=b, f=e))
