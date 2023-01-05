377abc294f737d8e23c772fc4493c69e3f9fc087
Azure0922 <maryam44keks@gmail.com>
2023-01-05 09:40:54 -0800

    calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+c = add(a, b)
+d = sub(a, b)
+e = mul(a, b)
+f = div(a, b)
+if __name__ == '__main__':
+    print("{g} + {h} = {i}".format(g=a, h=b, i=c))
+    print("{g} - {h} = {i}".format(g=a, h=b, i=d))
+    print("{g} * {h} = {i}".format(g=a, h=b, i=e))
+    print("{g} / {h} = {i}".format(g=a, h=b, i=f))
