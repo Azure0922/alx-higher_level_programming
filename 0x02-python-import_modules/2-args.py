f1131e4167c04a5f5ea468fe987121072c114a09
Azure0922 <maryam44keks@gmail.com>
2023-01-05 13:17:58 -0800

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    x = len(sys.argv)
+    if x == 1:
+        print("0 arguments.")
+    elif x > 1:
+        print(x - 1, " arguments:")
+        for i in range(1, x):
+            print(i, " : ", str(sys.argv[i]))
