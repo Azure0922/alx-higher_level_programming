fdbbc5eba6902e619414cfde6b3d107c95d09fab
Azure0922 <maryam44keks@gmail.com>
2023-01-05 12:44:50 -0800

    variable

--- 0x02-python-import_modules/0-import_add.py
+++ 0x02-python-import_modules/0-import_add.py
@@ -1,1 +1,1 @@
-__import__("0-add")
+__import__("5-variable_load")
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,4 +2,4 @@
 import variable_load_5
 c = variable_load_5.a
 if __name__ == '__main__':
-    print(c)
+    print("{d}".format(d=c))
