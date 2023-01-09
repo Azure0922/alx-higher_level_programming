d3a0beef0972048026626a8768c4362f997f57b8
Azure0922 <maryam44keks@gmail.com>
2023-01-09 11:48:24 -0800

    main

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     y = len(my_list)
-    if idx < y + 1 and idx > 0:
-        del my_list[idx]
+    if idx < y and idx > -1:
+        my_list[idx] = []
+        my_list
     else:
         return my_list
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     y = len(my_list)
-    if idx < 0:
-        return None
-    elif idx > y - 1:
+    if idx < 0 or idx > y - 1:
         return None
     else:
         my_list[idx] = element
