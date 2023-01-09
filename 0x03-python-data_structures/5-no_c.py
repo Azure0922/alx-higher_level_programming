7b6f0a454a920405833eb05b67c5bd2ef8965158
Azure0922 <maryam44keks@gmail.com>
2023-01-09 12:37:07 -0800

    noCc

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if my_string[i] = 'c' or my_string[i] = 'C':
-            my_string[i] = ''
+    s = my_string.translate({ord(i): None for i in 'c'})
+    t = s.translate({ord(i): None for i in 'C'})
+    return t
