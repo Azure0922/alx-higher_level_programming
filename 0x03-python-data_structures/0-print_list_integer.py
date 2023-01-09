29dcb977cede42c0fa0cee3cf34e14ca17598199
Azure0922 <maryam44keks@gmail.com>
2023-01-09 12:03:39 -0800

    printint

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    y = len(my_list)
-    for i in range(0, y):
-        m = my_list[i]
-        print("{a}".format(m))
+    print("{a}".format(a=*my_list), sep= '\n')
